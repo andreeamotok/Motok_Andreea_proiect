77de8100f1d22e3e071c3dcf018f34f9f9bba4d2
Larisa Gabriela Naznean <nazneanlarisa@gmail.com>
2020-12-04 21:10:57 +0200

    Modificaari

--- Models/Book.cs
+++ Models/Book.cs
@@ -8,18 +8,24 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace Naznean_Larisa_Lab8.Models
 {
     public class Book
-    {
+{ 
         public int ID { get; set; }
+        [Required, StringLength(150, MinimumLength = 3)]
         [Display(Name = "Book Title")]
         public string Title { get; set; }
+        [RegularExpression(@"^[A-Z][a-z]+\s[A-Z][a-z]+$", ErrorMessage = "Numele autorului trebuie sa fie de forma 'Prenume Nume'"), Required, StringLength(50, MinimumLength = 3)]
+
+
         public string Author { get; set; }
+        [Range(1, 300)]
         [Column(TypeName = "decimal(6, 2)")]
         public decimal Price { get; set; }
+
         [DataType(DataType.Date)]
         public DateTime PublishingDate { get; set; }
-        public int PublisherID { get; set; }
-        public Publisher Publisher { get; set; }
-        public ICollection<BookCategory> BookCategories { get; set; }
 
+        public int PublisherID { get; set; }
+        public Publisher Publisher { get; set; }  //navigation property
+        public ICollection<BookCategory> BookCategories { get; set; }
     }
 }
